657af0d4192a99702194a1b83ecb690213d68c00
yangxq <yangxq@yjhealth.cn>
2020-05-05 13:10:42 +0800

    添加注释说明

--- ts-main-test/test-child/vue.config.js
+++ ts-main-test/test-child/vue.config.js
@@ -1,11 +1,11 @@
 const StatsPlugin = require("stats-webpack-plugin");
 module.exports = {
   css: {
-    extract: false,
+    extract: false, //不打包css为单独文件
   },
   publicPath: "http://localhost:8081/",
   configureWebpack: {
-    devtool: "none",
+    devtool: "none", //去掉打包的map文件
     output: {
       library: "vueChild",
       libraryTarget: "window",
@@ -22,6 +22,7 @@ module.exports = {
         exclude: [/node_modules/],
       }),
     ],
+    // 开发环境跨域问题
     devServer: {
       headers: { "Access-Control-Allow-Origin": "*" },
     },
