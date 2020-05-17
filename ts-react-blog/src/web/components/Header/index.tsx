c34efe9efee4c225021c84af320d8d69fd9ad94b
yangxq <yangxq@yjhealth.cn>
2020-05-17 18:48:08 +0800

    添加文章

--- ts-react-blog/src/web/components/Header/index.tsx
+++ ts-react-blog/src/web/components/Header/index.tsx
@@ -1,8 +1,10 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import { NavLink } from "react-router-dom";
 import SearchInput from "../SearchInput/index";
 import "./index.less";
 import defaultLogo from "../../assets/images/logo.png";
+import { Button, Menu, Dropdown } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 export type navType = {
   name: string;
@@ -14,6 +16,17 @@ export interface Iprops {
   [propsKey: string]: any;
 }
 function Header(props: Iprops) {
+  const [isLogined, setLoginStatus]: [boolean, any] = useState(false);
+  const userMenu = (
+    <Menu>
+      <Menu.Item>
+        <a>个人信息</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a>我的发表</a>
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <div className="blog-header ">
       <div className="blog-main-width">
@@ -31,6 +44,22 @@ function Header(props: Iprops) {
         )}
         {props.search && <SearchInput placeholder="输入关键词搜索" />}
         {props.login}
+        {isLogined ? (
+          <div className="blog-logined">
+            <Dropdown overlay={userMenu} placement="bottomRight">
+              <div className="blog-user">
+                个人中心
+                <DownOutlined />
+              </div>
+            </Dropdown>
+            <Button type="primary">写文章</Button>
+          </div>
+        ) : (
+          <div className="blog-unlogined">
+            <Button>立即登录</Button>
+            <Button type="primary">免费注册</Button>
+          </div>
+        )}
       </div>
     </div>
   );
