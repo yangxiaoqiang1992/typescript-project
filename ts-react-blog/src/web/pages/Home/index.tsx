beffc9e78d5d7291db659fc3d08fcdcbadf29a30
yangxq <yangxq@yjhealth.cn>
2020-05-19 09:22:48 +0800

    首页

--- ts-react-blog/src/web/pages/Home/index.tsx
+++ ts-react-blog/src/web/pages/Home/index.tsx
@@ -14,6 +14,15 @@ const data: any = [
     author: "13333",
     createDate: new Date(),
   },
+  {
+    id: 2,
+    title: "前端安全之防范XSS实战小结",
+    content:
+      "由于前段时间业务有接触到富文本编辑器，且编辑器由用户直接使用，所以不可避免需要对其涉及到的XSS防护有所了解，因此对XSS防护做一个实战小结。",
+    favor: 122,
+    author: "13333",
+    createDate: new Date(),
+  },
 ];
 export default class Home extends React.Component<Ipros, Itate> {
   render() {
